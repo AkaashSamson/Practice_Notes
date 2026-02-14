6c5beb0388fe2bd72a1a9653a07847bd7af67b3e
AkaashSamson <akaashsam22@gmail.com>
2026-02-14 10:06:05 +0530

    setup marimo

---
+++ Python/marimo/first.py
@@ -0,0 +1,18 @@
+import marimo
+
+__generated_with = "0.19.10"
+app = marimo.App(width="medium", auto_download=["html", "ipynb"])
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
